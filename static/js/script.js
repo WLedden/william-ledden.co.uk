4387f02cc5ecd0f05718d3de1644f1c5cf5f99d7
Will Ledden <115587082+WLedden@users.noreply.github.com>
2025-11-19 18:11:53 +0000

    setup

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  const checkbox = document.getElementById("checkbox");
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark")
+  })
+}
